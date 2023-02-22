7056851a2e7594a93daf9a01b6ca1ca6e1fc20a9
Md. Nazmul Hossain Shakil <86412058+NHShakil@users.noreply.github.com>
2023-02-22 18:00:05 +0600

    updated_22_feb_23

--- scan_eeprom_data/scan_eeprom_data.ino
+++ scan_eeprom_data/scan_eeprom_data.ino
@@ -1,50 +1,18 @@
 #include <Wire.h>
-
+#include "IDU_EE_data.h"
+#include "ODU_EE_data.h"
 #define ADDR_Ax 0b000  //A2, A1, A0
 #define ADDR (0b1010 << 3) + ADDR_Ax
+#define IDU_ARR_LENGTH 255
+#define ODU_ARR_LENGTH 512
 
-short chkSum = 0, data = 0;
+short chkSum = 0, data = 0,arr_counter = ODU_ARR_LENGTH;
 
-//char ee_data [1024] = {326a,0c01,0a00,0170,5a70,685f,5a5a,3287,5a87,7668,5a55,5500,0028,281e,3034,2621,1c17,1322,1d18,130f,0100,0104,0bb8,0000,0000,0080,0000,0000,0000,0000,0000,0000,0600,0000,0000,0000,0000,0000,0000,0000,0000,0000,0000,0000,00ff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff,ffff};
-char ee_data[1024] = {
-  0X23, 0X6a, 0X1b, 0X01, 0X0a, 0X00, 0X01, 0X78, 0X64,
-  0X82, 0X7d, 0X6e, 0X64, 0X55, 0X46, 0X82, 0X64, 0X82,
-  0X7d, 0X6e, 0X64, 0X55, 0X50, 0X00, 0X00, 0X28, 0X28,
-  0X1e, 0X30, 0X34, 0X26, 0X21, 0X1c, 0X17, 0X13, 0X22,
-  0X1d, 0X18, 0X13, 0X0f, 0X01, 0X00, 0X00, 0X04, 0X0b,
-  0Xb8, 0X00, 0X00, 0X00, 0X00, 0X00, 0Xe8, 0X00, 0X00,
-  0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00,
-  0X00, 0Xe0, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00,
-  0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00,
-  0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0Xff,
-  0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff,
-  0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff,
-  0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff,
-  0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff,
-  0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff,
-  0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff,
-  0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff,
-  0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff,
-  0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff,
-  0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff,
-  0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff,
-  0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff,
-  0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff,
-  0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff,
-  0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff,
-  0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff,
-  0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff,
-  0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff,
-  0Xff, 0Xff, 0Xff, 0Xff
-};
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   Wire.begin();
-  //writeI2CByte(0, 1);
-
-  write_data(ee_data);
+  write_data(ee_odu_data);
   delay(3000);
   read_data();
   delay(2000);
@@ -73,7 +41,7 @@ byte readI2CByte(byte data_addr) {
   return data;
 }
 void read_data() {
-  for (int i = 0; i < 256; i++) {
+  for (int i = 0; i < arr_counter; i++) {
 
     Serial.print("ADD: ");
     Serial.print(i);
@@ -92,8 +60,10 @@ void write_data(char data[]) {
 
   int arr_size = sizeof(data) / sizeof(data[0]);
 
-  for (int j = 0; j < 256; j++) {
+  for (int j = 0; j < arr_counter; j++) {
     //Serial.println(data[i]);
     writeI2CByte(j, data[j]);
+    Serial.println(chkSum, HEX);
+    delay(10);
   }
 }
